796ed062d60c3034db30282652cea920b1af0b75
Ansh Patel <anshhmt2004@gmail.com>
2026-07-18 09:55:08 +0530

    feat: Configured MongoDB connection
    Connected to MongoDB Cloud , Configured Enviroment Variables and successfully Tested DB Connection with the app

---
+++ backend/src/config/database.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export async function connectDB(): Promise<void> {
+    console.log("Connecting to MongoDB...");
+    try {
+        await mongoose.connect(process.env.MONGODB_URI as string);
+        console.log("✅ Connected to MongoDB");
+    } catch (error) {
+        console.error("❌ Failed to connect to MongoDB:", error);
+        process.exit(1);
+    }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,20 @@
-import dotenv from "dotenv";
-import app from "./app";
 
+import dotenv from "dotenv";
 dotenv.config();
+
+
+import app from "./app";
+import { connectDB } from "./config/database";
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+async function startServer() {
+
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+startServer();
